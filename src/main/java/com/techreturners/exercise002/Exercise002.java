94f5b045ac8feae9e994edc4d721ec3aaa27277c
sanae <sanae.k1986@gmail.com>
2020-09-06 20:22:36 +0100

    exercice2

--- src/main/java/com/techreturners/exercise002/Exercise002.java
+++ src/main/java/com/techreturners/exercise002/Exercise002.java
@@ -2,12 +2,22 @@ package com.techreturners.exercise002;
 
 public class Exercise002 {
 
-    public boolean isFromManchester(/* Person person */) {
-        return false;
+    public boolean isFromManchester(Person person) {
+        if(person.getCity()=="Manchester"){
+            return true;
+        }else
+        {
+            return false;
+        }
+        
     }
 
-    public boolean canWatchFilm(/* Person person, int ageLimit*/) {
+    public boolean canWatchFilm( Person person, int ageLimit) {
+        if(person.getAge()<ageLimit){
         return false;
+        }else{
+            return true;
+        }
     }
     
 }
